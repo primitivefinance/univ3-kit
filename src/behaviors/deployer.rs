efcb897ee8ee16f6a70ee71bd4e80012ed1b4b61
ts0yu <120932697+ts0yu@users.noreply.github.com>
2024-02-23 20:06:25 +0000

    feat: abstract token deployment -> token_admin

--- src/behaviors/deployer.rs
+++ src/behaviors/deployer.rs
@@ -2,7 +2,7 @@
 use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
-use arbiter_bindings::bindings::{arbiter_token::ArbiterToken, liquid_exchange::LiquidExchange};
+use arbiter_bindings::bindings::liquid_exchange::LiquidExchange;
 use arbiter_core::middleware::ArbiterMiddleware;
 use arbiter_engine::{
     machine::{Behavior, EventStream},
@@ -15,27 +15,15 @@ use crate::bindings::uniswap_v3_factory::UniswapV3Factory;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct DeploymentData {
-    token_0: H160,
-    token_1: H160,
     factory: H160,
     liquid_exchange: H160,
-    pool: H160,
 }
 
 impl DeploymentData {
-    pub fn new(
-        token_0: H160,
-        token_1: H160,
-        factory: H160,
-        liquid_exchange: H160,
-        pool: H160,
-    ) -> Self {
+    pub fn new(factory: H160, liquid_exchange: H160) -> Self {
         Self {
-            token_0,
-            token_1,
             factory,
             liquid_exchange,
-            pool,
         }
     }
 }
@@ -50,28 +38,12 @@ impl Behavior<()> for Deployer {
         client: Arc<ArbiterMiddleware>,
         messager: Messager,
     ) -> Result<Option<EventStream<()>>> {
-        let token_0 =
-            ArbiterToken::deploy(client.clone(), ("Token 0".to_owned(), "TKN".to_owned(), 18))?
-                .send()
-                .await?;
-        let token_1 = ArbiterToken::deploy(
-            client.clone(),
-            ("Token 1".to_owned(), "TKN0".to_owned(), 18),
-        )?
-        .send()
-        .await?;
-
         let factory = deploy_factory(&client).await?;
         let liquid_exchange = deploy_liquid_exchange(&client).await?;
 
-        let pool = create_pool(&factory, token_0.address(), token_1.address()).await?;
-
         let deployment_data = DeploymentData {
-            token_0: token_0.address(),
-            token_1: token_1.address(),
             factory: factory.address(),
             liquid_exchange: liquid_exchange.address(),
-            pool,
         };
 
         messager
@@ -80,21 +52,6 @@ impl Behavior<()> for Deployer {
 
         Ok(None)
     }
-}
-
-async fn deploy_token(
-    client: Arc<ArbiterMiddleware>,
-    name: &str,
-    symbol: &str,
-) -> Result<ArbiterToken<ArbiterMiddleware>> {
-    let token = ArbiterToken::deploy(
-        client.clone(),
-        (String::from(name), String::from(symbol), 18_u8),
-    )?
-    .send()
-    .await?;
-
-    Ok(token)
 }
 
 async fn deploy_factory(
@@ -115,15 +72,4 @@ async fn deploy_liquid_exchange(
         .send()
         .await
         .map_err(|e| anyhow!("Failed to send liquid exchange: {}", e))
-}
-
-async fn create_pool<M>(factory: &UniswapV3Factory<M>, token_0: H160, token_1: H160) -> Result<H160>
-where
-    M: ethers::providers::Middleware,
-{
-    factory
-        .create_pool(token_0, token_1, 100)
-        .call()
-        .await
-        .map_err(|e| anyhow!("Failed to create pool: {}", e))
 }
