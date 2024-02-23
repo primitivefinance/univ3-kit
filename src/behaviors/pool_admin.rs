7d0a02c4c297de6e23c7a48dd673f3b42b4e2829
ts0yu <120932697+ts0yu@users.noreply.github.com>
2024-02-23 22:02:13 +0000

    feat: tests

--- src/behaviors/pool_admin.rs
+++ src/behaviors/pool_admin.rs
@@ -26,10 +26,10 @@ pub enum PoolAdminQuery {
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct PoolCreation {
-    factory: H160,
-    token_0: H160,
-    token_1: H160,
-    fee: u32,
+    pub factory: H160,
+    pub token_0: H160,
+    pub token_1: H160,
+    pub fee: u32,
 }
 
 #[async_trait::async_trait]
@@ -69,5 +69,79 @@ impl Behavior<Message> for PoolAdmin {
                 Ok(ControlFlow::Continue)
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use arbiter_engine::{agent::Agent, world::World};
+    use tracing_subscriber::FmtSubscriber;
+
+    use crate::bindings::uniswap_v3_factory::UniswapV3Factory;
+    use arbiter_bindings::bindings::arbiter_token::ArbiterToken;
+    use arbiter_engine::messager::To;
+
+    use crate::behaviors::pool_admin::{PoolAdmin, PoolCreation};
+
+    #[tokio::test]
+    async fn pool_admin_behaviour_test() {
+        let subscriber = FmtSubscriber::new();
+
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("setting default subscriber failed");
+
+        let mut world = World::new("univ3");
+
+        let factory = UniswapV3Factory::deploy(world.client.clone(), ())
+            .unwrap()
+            .send()
+            .await
+            .unwrap();
+
+        let token_0 = ArbiterToken::deploy(
+            client.clone(),
+            (String::from("0"), String::from("0"), 18_u8),
+        ).unwrap()
+        .send()
+        .await.unwrap();
+
+        let token_1 = ArbiterToken::deploy(
+            client.clone(),
+            (String::from("1"), String::from("1"), 18_u8),
+        ).unwrap()
+        .send()
+        .await.unwrap();
+
+        // messager and client are initialized later.
+        let pool_admin_behaviour = PoolAdmin {
+            messager: None,
+            client: None,
+        };
+
+        let pool_creation_request = PoolCreation {
+            factory: factory.address(),
+            token_0: token_0.address(),
+            token_1: token_1.address(),
+            fee: 100,
+        };
+
+        let agent = Agent::builder("pool_admin");
+
+        world.add_agent(agent.with_behavior(pool_admin_behaviour));
+
+        let messager = world.messager.clone();
+
+        messager
+            .send(To::All, serde_json::to_string(&pool_creation_request).unwrap())
+            .await.unwrap();
+
+        world.run().await.expect("world failed to run");
+
+        // world
+        //     .environment
+        //     .db
+        //     .accounts
+        //     .get(&factory.address())
+        //     .expect("factory should be deployed");
     }
 }
