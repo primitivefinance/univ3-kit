3ced83cc2aad3bf5824bcfaf0de545db246584b1
Colin Roberts <colin@primitive.xyz>
2024-02-23 10:15:46 -0700

    feat: using main macro (#14)
    * feat: using main macro
    
    There are some debug prints in here right now as I'm seeing that this is not running properly.
    
    Revert "feat: using main macro"
    
    This reverts commit 78c0032d1f3551020c7c5e582b2f243f96b3bbb5.
    
    feat: using main macro
    
    There are some debug prints in here right now as I'm seeing that this is not running properly.
    
    * feat: fix clippy and fmt
    
    * nit: fmt
    
    * nit: fmt
    
    ---------
    
    Co-authored-by: ts0yu <120932697+ts0yu@users.noreply.github.com>

--- src/behaviors/deployer.rs
+++ src/behaviors/deployer.rs
@@ -1,17 +1,18 @@
+#![allow(dead_code)]
 use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
+use arbiter_bindings::bindings::{arbiter_token::ArbiterToken, liquid_exchange::LiquidExchange};
 use arbiter_core::middleware::ArbiterMiddleware;
 use arbiter_engine::{
     machine::{Behavior, EventStream},
     messager::{Messager, To},
 };
 use ethers::types::H160;
+use tracing::debug;
 
 use super::*;
-use crate::bindings::{
-    liquid_exchange::LiquidExchange, token::ArbiterToken, uniswap_v3_factory::UniswapV3Factory,
-};
+use crate::bindings::uniswap_v3_factory::UniswapV3Factory;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct DeploymentData {
@@ -41,7 +42,7 @@ impl DeploymentData {
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct Deployer;
+pub struct Deployer {}
 
 #[async_trait::async_trait]
 impl Behavior<()> for Deployer {
@@ -50,15 +51,24 @@ impl Behavior<()> for Deployer {
         client: Arc<ArbiterMiddleware>,
         messager: Messager,
     ) -> Result<Option<EventStream<()>>> {
-        let token_0 = self.deploy_token(&client, "Token 0", "0").await?;
-        let token_1 = self.deploy_token(&client, "Token 1", "1").await?;
+        println!("Prior to token deployment.");
+        let token_0 =
+            ArbiterToken::deploy(client.clone(), ("Token 0".to_owned(), "TKN".to_owned(), 18))?
+                .send()
+                .await?;
+        println!("Deployed token 0");
+        let token_1 = ArbiterToken::deploy(
+            client.clone(),
+            ("Token 1".to_owned(), "TKN0".to_owned(), 18),
+        )?
+        .send()
+        .await?;
+        println!("Deployed tokens");
+        let factory = deploy_factory(&client).await?;
+        let liquid_exchange = deploy_liquid_exchange(&client).await?;
 
-        let factory = self.deploy_factory(&client).await?;
-        let liquid_exchange = self.deploy_liquid_exchange(&client).await?;
-
-        let pool = self
-            .create_pool(&factory, token_0.address(), token_1.address())
-            .await?;
+        let pool = create_pool(&factory, token_0.address(), token_1.address()).await?;
+        println!("Got here.");
 
         let deployment_data = DeploymentData {
             token_0: token_0.address(),
@@ -68,66 +78,60 @@ impl Behavior<()> for Deployer {
             pool,
         };
 
-        let _ = messager
+        println!("Deployment data: {:?}", deployment_data);
+        messager
             .send(To::All, serde_json::to_string(&deployment_data)?)
-            .await;
+            .await?;
+        debug!("Sent deployment data: {:?}", deployment_data);
 
         Ok(None)
     }
 }
 
-impl Deployer {
-    async fn deploy_token(
-        &self,
-        client: &Arc<ArbiterMiddleware>,
-        name: &str,
-        symbol: &str,
-    ) -> Result<ArbiterToken<ArbiterMiddleware>> {
-        ArbiterToken::deploy(
-            client.clone(),
-            (String::from(name), String::from(symbol), 18),
-        )
-        .map_err(|e| anyhow!("Failed to deploy token {}: {}", name, e))?
+async fn deploy_token(
+    client: Arc<ArbiterMiddleware>,
+    name: &str,
+    symbol: &str,
+) -> Result<ArbiterToken<ArbiterMiddleware>> {
+    println!("In here.");
+    let thing = ArbiterToken::deploy(
+        client.clone(),
+        (String::from(name), String::from(symbol), 18),
+    )?
+    .send()
+    .await;
+    println!("Thing: {:?}", thing);
+
+    Ok(thing?)
+}
+
+async fn deploy_factory(
+    client: &Arc<ArbiterMiddleware>,
+) -> Result<UniswapV3Factory<ArbiterMiddleware>> {
+    UniswapV3Factory::deploy(client.clone(), ())
+        .map_err(|e| anyhow!("Failed to deploy factory: {}", e))?
         .send()
         .await
-        .map_err(|e| anyhow!("Failed to send token {}: {}", name, e))
-    }
+        .map_err(|e| anyhow!("Failed to send factory deployment: {}", e))
+}
 
-    async fn deploy_factory(
-        &self,
-        client: &Arc<ArbiterMiddleware>,
-    ) -> Result<UniswapV3Factory<ArbiterMiddleware>> {
-        UniswapV3Factory::deploy(client.clone(), ())
-            .map_err(|e| anyhow!("Failed to deploy factory: {}", e))?
-            .send()
-            .await
-            .map_err(|e| anyhow!("Failed to send factory deployment: {}", e))
-    }
+async fn deploy_liquid_exchange(
+    client: &Arc<ArbiterMiddleware>,
+) -> Result<LiquidExchange<ArbiterMiddleware>> {
+    LiquidExchange::deploy(client.clone(), ())
+        .map_err(|e| anyhow!("Failed to deploy liquid exchange: {}", e))?
+        .send()
+        .await
+        .map_err(|e| anyhow!("Failed to send liquid exchange: {}", e))
+}
 
-    async fn deploy_liquid_exchange(
-        &self,
-        client: &Arc<ArbiterMiddleware>,
-    ) -> Result<LiquidExchange<ArbiterMiddleware>> {
-        LiquidExchange::deploy(client.clone(), ())
-            .map_err(|e| anyhow!("Failed to deploy liquid exchange: {}", e))?
-            .send()
-            .await
-            .map_err(|e| anyhow!("Failed to send liquid exchange: {}", e))
-    }
-
-    async fn create_pool<M>(
-        &self,
-        factory: &UniswapV3Factory<M>,
-        token_0: H160,
-        token_1: H160,
-    ) -> Result<H160>
-    where
-        M: ethers::providers::Middleware,
-    {
-        factory
-            .create_pool(token_0, token_1, 100)
-            .call()
-            .await
-            .map_err(|e| anyhow!("Failed to create pool: {}", e))
-    }
+async fn create_pool<M>(factory: &UniswapV3Factory<M>, token_0: H160, token_1: H160) -> Result<H160>
+where
+    M: ethers::providers::Middleware,
+{
+    factory
+        .create_pool(token_0, token_1, 100)
+        .call()
+        .await
+        .map_err(|e| anyhow!("Failed to create pool: {}", e))
 }
--- src/behaviors/mod.rs
+++ src/behaviors/mod.rs
@@ -1,5 +1,11 @@
+use arbiter_engine::machine::*;
+use arbiter_macros::Behaviors;
 use serde::{Deserialize, Serialize};
 
 pub mod deployer;
+use deployer::Deployer;
 
-pub use deployer::Deployer;
+#[derive(Behaviors, Debug, Serialize, Deserialize)]
+pub enum Behaviors {
+    Deployer(Deployer),
+}
